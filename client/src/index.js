3e89f73c1974c10d5e952fd182a21b0439969aee
McKenna Lawson <mckenna.lawson01@gmail.com>
2023-02-28 22:09:06 -0600

    finished navbar

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/Navbar/navbar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
