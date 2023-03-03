f37bb19ec07571a5ded1592533f5837b135a2b40
Alex Clayton <alexander.clayton.email@gmail.com>
2023-03-02 20:10:00 -0600

    refactored the signup page to use the antd component

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,37 +1,20 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
-
 import Auth from "../utils/auth";
 
+import { Form, Input, Button, Checkbox } from "antd";
+
 const Signup = () => {
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
+  const [form] = Form.useForm();
   const [addProfile, { error, data }] = useMutation(ADD_USER);
 
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
   // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-
+  const handleFormSubmit = async (values) => {
+    console.log(values);
     try {
       const { data } = await addProfile({
-        variables: { ...formState },
+        variables: { ...values },
       });
 
       Auth.login(data.addProfile.token);
@@ -52,39 +35,72 @@ const Signup = () => {
                 <Link to="/">back to the homepage.</Link>
               </p>
             ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your username"
+              <Form
+                form={form}
+                onFinish={handleFormSubmit}
+                style={{ maxWidth: 600 }}
+                initialValues={{
+                  remember: true,
+                }}
+              >
+                <Form.Item
+                  label="Your username"
                   name="name"
-                  type="text"
-                  value={formState.name}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-info"
-                  style={{ cursor: "pointer" }}
-                  type="submit"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your username!",
+                    },
+                  ]}
                 >
-                  Submit
-                </button>
-              </form>
+                  <Input />
+                </Form.Item>
+                <Form.Item
+                  label="Your email"
+                  name="email"
+                  rules={[
+                    {
+                      type: "email",
+                      required: true,
+                      message: "Please input your email!",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item
+                  label="Your password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
+                <Form.Item
+                  name="remember"
+                  valuePropName="checked"
+                  wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                  }}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <Form.Item
+                  wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                  }}
+                >
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
             )}
 
             {error && (
