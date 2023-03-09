f7e6b5df28b168220db104112429a091f1c34543
Alex Clayton <alexander.clayton.email@gmail.com>
2023-03-09 14:51:14 -0600

    cleaned up home.js, broke it down into components

---
+++ client/src/components/EditProfile/EditProfile.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import AvatarAdd from '../UploadAvatar/UploadAvatar';
+
+const EditProfile = ({ name, setName, username, setUsername, bio, setBio, onSave, onCancel }) => {
+    const [imageUrl, setImageURL] = useState('');
+
+    const handleAvatarChange = (imageURL) => {
+        setImageURL(imageURL);
+      }
+  return (
+    <div>
+      <AvatarAdd onChange={handleAvatarChange} />
+      <label>Name:</label>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <label>Username:</label>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <label>Bio:</label>
+      <textarea value={bio} onChange={(e) => setBio(e.target.value)} />
+      <button onClick={onSave}>Save</button>
+      <button onClick={onCancel}>Cancel</button>
+    </div>
+  );
+};
+
+export default EditProfile;
---
+++ client/src/components/ImageArray/ImageArray.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Draggable from 'react-draggable';
+import { ResizableBox } from 'react-resizable';
+
+const ImageArray = ({ imageUrl }) => {
+  return (
+    <Draggable handle=".drag-handle">
+      <ResizableBox width={200} height={200} minConstraints={[50, 50]} maxConstraints={[600, 600]}>
+        <div style={{ position: 'relative' }}>
+          <div className="drag-handle" style={{ position: 'absolute', top: 0, left: 0, right: 0, height: '50px', cursor: 'move' }} />
+          <img src={imageUrl} style={{ width: '100%', height: '100%' }} />
+        </div>
+      </ResizableBox>
+    </Draggable>
+  );
+};
+
+export default ImageArray;
