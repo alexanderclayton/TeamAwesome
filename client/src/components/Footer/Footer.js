b9aeb34e7dfa4fc0dcb0cbeedb71152fa41d444a
svanmeter93 <107381935+svanmeter93@users.noreply.github.com>
2023-03-09 18:18:54 -0600

    footer edits

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -9,20 +9,19 @@ const Footer = () => {
 
   return (
     <footer className='footer'>
-      Project completed by: 
-      McKenna Lawson,
+      Project completed by:  
+      McKenna Lawson
       <span className="github-icon" onClick={() => handleGithubClick('https://github.com/mac-codes')}>
-        <GithubFilled className="mckenna" style={{ color: '#6f42c1', fontSize: 24, cursor: 'pointer' }} />
+        <GithubFilled className="mckenna" style={{ padding: "-5px", color: '#6f42c1', fontSize: 24, cursor: 'pointer' }} />
       </span> 
-       Alex Clayton, 
+       Alex Clayton
       <span className="github-icon" onClick={() => handleGithubClick('https://github.com/alexclaytonbootcamp')}>
-        <GithubFilled className="alex" style={{ color: '#6f42c1', fontSize: 24, cursor: 'pointer' }} />
+        <GithubFilled className="alex" style={{ color: '#6f42c1', fontSize: 24, cursor: 'pointer' , }} />
       </span> 
-      Seth VanMeter, 
+      Seth VanMeter
       <span className="github-icon" onClick={() => handleGithubClick('https://github.com/svanmeter93')}>
         <GithubFilled className="seth" style={{ color: '#6f42c1', fontSize: 24, cursor: 'pointer' }} />
       </span> 
-      and 
       Josh Torrence
       <span className="github-icon" onClick={() => handleGithubClick('https://github.com/DeadEye44')}>
         <GithubFilled className="josh" style={{ color: '#6f42c1', fontSize: 24, cursor: 'pointer' }} />
